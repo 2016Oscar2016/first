0fc56e379d8dbf4ca0b5fa07b5aaf64628e4d532
2016Oscar2016 <olexandr.pasternak@gmail.com>
2022-10-03 21:25:27 +0300

    Bug fix

--- database_1.py
+++ database_1.py
@@ -2,8 +2,8 @@ import sqlite3
 
 connection = sqlite3.connect("data.sl3", 5)
 cur = connection.cursor()
-#cur.execute("CREATE TABLE temperature (Temperature TEXT)")
-cur.execute("INSERT INTO temperature (Temperature) VALUES ('аа')")
+#cur.execute("CREATE TABLE Temp_Date_3 (Id INTEGER NOT NULL PRIMARY KEY,Temperature TEXT, Date TEXT)")
+cur.execute("INSERT INTO Temp_Date_3 (Id, Date, Temperature) VALUES ('7', '09.10.2022', '16°C')")
 
 connection.commit()
 connection.close()
